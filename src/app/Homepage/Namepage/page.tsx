92069e957d91c2b931f75c9ddd31ae8482e21c7e
saeedasif576881 <saeedasif576881@gmail.com>
2024-03-28 22:33:19 +0500

    First Commit

---
+++ src/app/Homepage/Namepage/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Namepage() {
+  return (
+    <div>
+      <Link href="./">Go to Home Page</Link>
+      <h1>Name Page</h1>
+    </div>
+  );
+}
---
+++ src/app/Homepage/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Link href="./Homepage/Namepage">Go to Name Page</Link>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
